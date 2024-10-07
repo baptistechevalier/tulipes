4df90a00e4d469a542fc4afadf63526f9712dc76
Mathieu Mistretta <mathieu.mistretta@gmail.com>
2024-10-07 11:06:30 +0200

    css tulipe qui se balade

--- index.php
+++ index.php
@@ -7,6 +7,45 @@
 </head>
 <body>
     <?php include('header.php');?>
+    <div id="emoji">🌷</div>
     
+
+
+    <script>
+  const emoji = document.getElementById('emoji');
+  let isVisible = false;
+
+  function getRandomPosition() {
+    const x = Math.floor(Math.random() * window.innerWidth);
+    const y = Math.floor(Math.random() * window.innerHeight);
+    return { x, y };
+  }
+
+  function moveEmoji() {
+    const { x, y } = getRandomPosition();
+    emoji.style.left = `${x}px`;
+    emoji.style.top = `${y}px`;
+  }
+
+  function showAndMoveEmoji() {
+    if (!isVisible) {
+      emoji.style.display = 'block';
+      isVisible = true;
+    }
+    moveEmoji();
+  }
+
+  // Faire apparaître le 🌷 toutes les minutes
+  setInterval(() => {
+    emoji.style.display = 'none';
+    isVisible = false;
+
+    // Après 1 minute (60000 ms), réapparaît et commence à se déplacer
+    setTimeout(showAndMoveEmoji, 60000);
+  }, 60000);
+
+  // Déplacer l'emoji constamment
+  setInterval(moveEmoji, 1000);
+</script>    
 </body>
 </html>
