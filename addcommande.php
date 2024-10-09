ebd2aa17282b44ae079c97c122340e29d52708e5
Baptiste Chevalier <baptistechvlr62@gmail.com>
2024-10-09 14:28:56 +0200

    AA

--- addcommande.php
+++ addcommande.php
@@ -28,83 +28,90 @@
     <?php include(__DIR__ . '/partials/header.php'); ?>
     <div class="tulipe-container"></div>
     <script src="script.js"></script>
-    <main>
-        <form action="/tulipe/addcommandeaction.php" method="post" name="add">
-		<table width="25%" border="0">
-				<tr> 
-					<td>Nom et Prénom : </td>
-					<td><input type="text" name="nom"></td>
-				</tr>
-				<tr>
-					<td>Civilité :</td>
-					<td><select id="civilite" name="civilite">
-						<option value="option">~Choisir une option~</option>
-						<option value="M">M.</option>
-						<option value="Mme">Mme</option>
-						<option value="autre">Autre</option>
-					</select></td>
-				</tr>
-				<tr>
-					<td>Quantité  : </td>
-					<td><input type="int" name="quantite"></td>
-				</tr>
-				<tr> 
-					<td>Semaine de livraison :</td>
-					<td><select id="livraison" name="livraison">
-						<option value="option">~Choisir une option~</option>
-						<option value="1">1</option>
-						<option value="2">2</option>
-						<option value="3">3</option>
-						<option value="S1 et S2">1 et 2</option>
-						<option value = "S1 et S3">1 et 3</option>
-						<option value = "S2 et S3">2 et 3</option>
-						<option value = "S1, S2, et S3">1 ; 2 et 3</option>
-					</td>
-				</tr>
-				<tr>
-					<td>Adresse de livraison :</td>
-					<td><input type = "text" name = "adresse_liv"></td>
-				</tr>
-				<tr>
-					<td>Adresse de la personne :</td>
-					<td><input type = "text" name = "adresse_pers"></td>
-				</tr>
-                <tr>
-                    <td>Moyen de réglement :</td>
-                    <td><select id="reglement" name="reglement">
-						<option value="option">~Choisir une option~</option>
-						<option value="cheque">Chèque</option>
-						<option value="espèce">Espèce</option>
-						</select>
-					</td>
-                </tr>
-				<tr>
-					<td>Commande pris en charge par :</td>
-					<td><input type="text" name="vendupar"></td>
-				</tr>
-				<tr>
-					<td>Numéro de téléphone :</td>
-					<td><input type="text" name="num"></td>
-				</tr>
-				<tr>
-					<td>Email :</td>
-					<td><input type="mail" name="mail"></td>
-				</tr>
-				<tr>
-					<td>Signature :</td>
-					<td>
-						<canvas id="signaturePad"></canvas>
-						<br>
-						<button type="button" onclick="clearSignature()">Effacer</button>						
-						<input type="hidden" name="signature" id="signatureInput">
-					</td>
-				</tr>
-				<tr>
-					<td></td>
-					<td><input type="submit" name="submit" value="Ajouter"></td>
-				</tr>
-			</table>
-		</form>
+    <main class="container my-5">
+        <div class="card shadow">
+            <div class="card-body">
+                <h2 class="card-title text-center mb-4">Ajouter une Commande</h2>
+                <form action="/tulipe/addcommandeaction.php" method="post" name="add">
+                    <div class="form-group">
+                        <label for="nom">Nom et Prénom :</label>
+                        <input type="text" class="form-control" id="nom" name="nom" required>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="civilite">Civilité :</label>
+                        <select class="form-control" id="civilite" name="civilite" required>
+                            <option value="">~Choisir une option~</option>
+                            <option value="M">M.</option>
+                            <option value="Mme">Mme</option>
+                            <option value="autre">Autre</option>
+                        </select>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="quantite">Quantité :</label>
+                        <input type="number" class="form-control" id="quantite" name="quantite" min="1" required>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="livraison">Semaine de livraison :</label>
+                        <select class="form-control" id="livraison" name="livraison" required>
+                            <option value="">~Choisir une option~</option>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="S1 et S2">1 et 2</option>
+                            <option value="S1 et S3">1 et 3</option>
+                            <option value="S2 et S3">2 et 3</option>
+                            <option value="S1, S2, et S3">1, 2 et 3</option>
+                        </select>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="adresse_liv">Adresse de livraison :</label>
+                        <input type="text" class="form-control" id="adresse_liv" name="adresse_liv" required>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="adresse_pers">Adresse de la personne :</label>
+                        <input type="text" class="form-control" id="adresse_pers" name="adresse_pers" required>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="reglement">Moyen de règlement :</label>
+                        <select class="form-control" id="reglement" name="reglement" required>
+                            <option value="">~Choisir une option~</option>
+                            <option value="cheque">Chèque</option>
+                            <option value="especes">Espèces</option>
+                        </select>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="vendupar">Commande prise en charge par :</label>
+                        <input type="text" class="form-control" id="vendupar" name="vendupar" required>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="num">Numéro de téléphone :</label>
+                        <input type="tel" class="form-control" id="num" name="num" pattern="[0-9]{10}" placeholder="Ex: 0123456789" required>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="mail">Email :</label>
+                        <input type="email" class="form-control" id="mail" name="mail" required>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="signaturePad">Signature :</label>
+                        <canvas id="signaturePad"></canvas>
+                        <button type="button" class="btn btn-secondary btn-clear" onclick="clearSignature()">Effacer</button>
+                        <input type="hidden" name="signature" id="signatureInput">
+                    </div>
+
+                    <button type="submit" class="btn btn-primary btn-block">Ajouter</button>
+                </form>
+            </div>
+        </div>
     </main>
     <?php include('./partials/footer.php') ?>
 
