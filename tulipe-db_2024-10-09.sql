1cbc4b1801ecf5ad2f3afda3e4406148d58d7544
Baptiste Chevalier <baptistechvlr62@gmail.com>
2024-10-09 13:52:27 +0200

    ajout new bdd

---
+++ tulipe-db_2024-10-09.sql
@@ -0,0 +1,153 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le :  mer. 09 oct. 2024 à 12:52
+-- Version du serveur :  10.3.17-MariaDB
+-- Version de PHP :  7.2.22
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `tulipe`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `commandes`
+--
+
+CREATE TABLE `commandes` (
+  `nom` varchar(200) NOT NULL,
+  `quantite` int(11) NOT NULL,
+  `reglement` varchar(150) NOT NULL,
+  `montant` int(11) NOT NULL,
+  `id_commande` int(11) NOT NULL,
+  `livraison` int(11) NOT NULL,
+  `signature` varchar(2500) NOT NULL,
+  `adresse_personne` varchar(2500) NOT NULL,
+  `adresse_de_livraison` varchar(2500) NOT NULL,
+  `vendupar` varchar(1000) NOT NULL,
+  `telephone` varchar(33) NOT NULL,
+  `mail` varchar(100) NOT NULL,
+  `civilite` varchar(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `commandes`
+--
+
+INSERT INTO `commandes` (`nom`, `quantite`, `reglement`, `montant`, `id_commande`, `livraison`, `signature`, `adresse_personne`, `adresse_de_livraison`, `vendupar`, `telephone`, `mail`, `civilite`) VALUES
+('Adolf Hytlair', 3, 'cheque', 30, 7, 3, '6706273394778.png', 'Berlin', 'Berlin', 'Joseph Staline', '33333333', 'adolfhytlair@gmail.com', 'M');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `roles`
+--
+
+CREATE TABLE `roles` (
+  `id_role` int(11) NOT NULL,
+  `nom_role` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `roles`
+--
+
+INSERT INTO `roles` (`id_role`, `nom_role`) VALUES
+(1, 'eleve'),
+(2, 'prof');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `id_users` int(11) NOT NULL,
+  `login` varchar(150) NOT NULL,
+  `mdp` varchar(150) NOT NULL,
+  `roles` int(11) DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `users`
+--
+
+INSERT INTO `users` (`id_users`, `login`, `mdp`, `roles`) VALUES
+(1, 'prof', 'prof', 2),
+(2, 'eleve', 'eleve', 1),
+(3, 'equipe 1', 'motdepasse', 1);
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `commandes`
+--
+ALTER TABLE `commandes`
+  ADD PRIMARY KEY (`id_commande`);
+
+--
+-- Index pour la table `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id_role`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id_users`),
+  ADD KEY `roles` (`roles`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `commandes`
+--
+ALTER TABLE `commandes`
+  MODIFY `id_commande` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT pour la table `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `id_role` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id_users` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`roles`) REFERENCES `roles` (`id_role`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
